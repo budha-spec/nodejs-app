a2299166372118d19e7fef2cd0aff2b59fdc4fc2
budha.jethava <budha.jethava@spec-india.com>
2026-09-10 14:44:49 +0530

    Added three files

---
+++ tasks.js
@@ -0,0 +1,14 @@
+const tasks = [
+  {
+    id: 1,
+    title: "Learn AWS CI/CD",
+    completed: false
+  },
+  {
+    id: 2,
+    title: "Learn Docker",
+    completed: true
+  }
+];
+
+module.exports = tasks;
